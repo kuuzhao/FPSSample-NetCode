088da57f5a234de062facd69c0c56b66796cbb88
liang.zhao <liang.zhao@unity3d.com>
2019-08-01 17:09:57 +0800

    [LZ] replicate the grenade GameObject on the client side.

---
+++ Assets/_NewNetwork/Client/Systems/GrenadeSystems.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+[DisableAutoCreation]
+[UpdateInGroup(typeof(ClientSimulationSystemGroup))]
+[AlwaysUpdateSystem]
+public class GrenadeGoSystem : ComponentSystem
+{
+    EntityQuery grenadeQuery;
+    EntityQuery grenadeGoQuery;
+
+    protected override void OnCreateManager()
+    {
+        grenadeQuery = GetEntityQuery(typeof(RepGrenadeTagComponentData));
+        grenadeGoQuery = GetEntityQuery(typeof(RepGrenadeGoCreatedTag));
+    }
+
+    protected override void OnUpdate()
+    {
+        if (ClientGameLoop.Instance == null || !ClientGameLoop.Instance.IsLevelLoaded())
+            return;
+
+        if (!ReplicatedPrefabMgr.IsInitialized())
+            return;
+
+        // grenade entities
+        var grenadeEntities = grenadeQuery.GetEntityArraySt();
+        for (int i = 0; i < grenadeEntities.Length; ++i)
+        {
+            var grenadeEnt = grenadeEntities[i];
+            if (!EntityManager.HasComponent<RepGrenadeGoCreatedTag>(grenadeEnt))
+            {
+                ReplicatedPrefabMgr.LoadPrefabIntoEntity("assets__newnetwork_prefab_robot_grenade", World, grenadeEnt);
+                EntityManager.AddComponentData(grenadeEnt, default(RepGrenadeGoCreatedTag));
+            }
+        }
+
+        // grenade GameObjects
+        var grenadeGoEntities = grenadeGoQuery.GetEntityArraySt();
+        for (int i = 0; i < grenadeGoEntities.Length; ++i)
+        {
+            var grenadeGoEnt = grenadeGoEntities[i];
+
+            var transform = EntityManager.GetComponentObject<Transform>(grenadeGoEnt);
+            var translation = EntityManager.GetComponentData<Translation>(grenadeGoEnt);
+            transform.position = translation.Value;
+        }
+    }
+}
